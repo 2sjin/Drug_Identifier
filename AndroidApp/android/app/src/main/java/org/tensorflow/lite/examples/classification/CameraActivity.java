ec28a217c4d85496795add39f8ccf6c2bbee9154
2sjin <options3224@gmail.com>
2022-06-09 14:30:53 +0900

    일부 기기 카메라 멈춤 현상 해결

--- AndroidApp/android/app/src/main/java/org/tensorflow/lite/examples/classification/CameraActivity.java
+++ AndroidApp/android/app/src/main/java/org/tensorflow/lite/examples/classification/CameraActivity.java
@@ -105,10 +105,20 @@ public abstract class CameraActivity extends AppCompatActivity
 
   private Button shutterButton;  // 셔터 버튼 선언 
 
+
+
+
+
   @Override
   protected void onCreate(final Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
     LOGGER.d("onCreate " + this);
-    super.onCreate(null);
+  }
+
+  @Override
+  protected void onResumeFragments() {
+    super.onResumeFragments();
+
     getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
 
     setContentView(R.layout.tfe_ic_activity_camera);
